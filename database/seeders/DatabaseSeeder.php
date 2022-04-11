0b7423041cacc090a661ea49c828b8b0003ff8e8
shamim2001 <shamimahmedridoy2016@gmail.com>
2022-04-11 12:43:49 +0600

    custom dashboard template dashboard integrate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Shamim Ahmed',
+            'username' => 'Coder',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('123'),
+            'email_verified_at' => now(),
+            'image' => 'https://picsum.photos/300'
+        ]);
     }
 }
---
+++ resources/views/admin/components/sidebar.blade.php
@@ -0,0 +1,167 @@
+ <!-- start sidebar -->
+  <div id="sideBar" class="relative flex flex-col flex-wrap bg-white border-r border-gray-300 p-6 flex-none w-64 md:-ml-64 md:fixed md:top-0 md:z-30 md:h-screen md:shadow-xl animated faster">
+
+
+    <!-- sidebar content -->
+    <div class="flex flex-col">
+
+      <!-- sidebar toggle -->
+      <div class="text-right hidden md:block mb-4">
+        <button id="sideBarHideBtn">
+          <i class="fad fa-times-circle"></i>
+        </button>
+      </div>
+      <!-- end sidebar toggle -->
+
+      <p class="uppercase text-xs text-gray-600 mb-4 tracking-wider">Users</p>
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+        <i class="fad fa-user-plus text-xs mr-2"></i>
+        Add User
+      </a>
+      <!-- end link -->
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit User
+      </a>
+      <!-- end link -->
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-red-600 transition ease-in-out duration-500">
+          <i class="fad fa-trash text-xs mr-2"></i>
+        Delete User
+      </a>
+      <!-- end link -->
+
+
+      <p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">Activity</p>
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit User
+      </a>
+      <!-- end link -->
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit
+      </a>
+      <!-- end link -->
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-red-600 transition ease-in-out duration-500">
+          <i class="fad fa-trash text-xs mr-2"></i>
+        Delete
+      </a>
+      <!-- end link -->
+
+
+      <p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">Category</p>
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit User
+      </a>
+      <!-- end link -->
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit
+      </a>
+      <!-- end link -->
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-red-600 transition ease-in-out duration-500">
+          <i class="fad fa-trash text-xs mr-2"></i>
+        Delete
+      </a>
+      <!-- end link -->
+
+
+<p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">Media</p>
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit User
+      </a>
+      <!-- end link -->
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit
+      </a>
+      <!-- end link -->
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-red-600 transition ease-in-out duration-500">
+          <i class="fad fa-trash text-xs mr-2"></i>
+        Delete
+      </a>
+      <!-- end link -->
+
+
+<p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">Problems</p>
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit User
+      </a>
+      <!-- end link -->
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit
+      </a>
+      <!-- end link -->
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-red-600 transition ease-in-out duration-500">
+          <i class="fad fa-trash text-xs mr-2"></i>
+        Delete
+      </a>
+      <!-- end link -->
+
+<p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">Solutions</p>
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit User
+      </a>
+      <!-- end link -->
+
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fad fa-pen text-xs mr-2"></i>
+        Edit
+      </a>
+      <!-- end link -->
+      <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-red-600 transition ease-in-out duration-500">
+          <i class="fad fa-trash text-xs mr-2"></i>
+        Delete
+      </a>
+      <!-- end link -->
+
+<p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">Tags</p>
+
+    <!-- link -->
+      <a href="" class="mb-3 capitalize font-medium text-sm hover:text-purple-600 transition ease-in-out duration-500">
+          <i class="fad fa-search text-xs mr-2"></i>
+        Search
+      </a>
+      <!-- end link -->
+
+
+    </div>
+    <!-- end sidebar content -->
+
+  </div>
+  <!-- end sidbar -->
