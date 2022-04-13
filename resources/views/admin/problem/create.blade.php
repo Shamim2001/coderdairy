7b7ba3efc19a2b61580b3061e3fef9fe3b0fce4b
shamim2001 <shamimahmedridoy2016@gmail.com>
2022-04-13 23:52:44 +0600

    problem create page

--- resources/views/admin/problem/create.blade.php
+++ resources/views/admin/problem/create.blade.php
@@ -11,27 +11,87 @@
                 <a href="{{ route('problem.index') }}" class="btn-shadow">Back</a>
             </div>
 
-            <form action="" method="POST" enctype="multipart/form-data">
-                    @csrf
 
-                    <div class="mt-6 flex">
-                        <div class="flex-1 mr-4">
-                            <label for="name" class="cursor-pointer block text-sm mb-1 ">Name</label>
-                            <input type="text" name="name" id="name" class="w-full border-teal-50 rounded-md" value="{{ old('name') }}">
+            <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
 
-                            @error('name')
-                                <p class="text-red-700 text-sm">{{ $message }}</p>
-                            @enderror
+                    <form action="" method="POST" enctype="multipart/form-data">
+                        @csrf
 
+                        <div class="mt-6 flex">
+                            <div class="flex-1 mr-4">
+                                <label for="name" class="cursor-pointer block text-sm mb-1">Name</label>
+                                <input type="text" name="name" id="name" class="w-full rounded border border-gray-600 py-2" value="{{ old('name')}}">
+
+                                @error('name')
+                                    <p class="text-red-700 text-sm">{{$message}}</p>
+                                @enderror
+
+                            </div>
+                            <div class="flex-1 mr-4">
+                                <label for="category" class="cursor-pointer block text-sm mb-1">Category</label>
+                                <input type="text" name="category" id="category" class="w-full rounded border border-gray-600 py-2" value="{{ old('category')}}">
+
+                                @error('category')
+                                    <p class="text-red-700 text-sm">{{$message}}</p>
+                                @enderror
+
+                            </div>
+                        </div>
+
+                        <div class="mt-6 flex justify-between">
+                            <div class="flex-1 mr-4">
+                                <label for="visibility" class="cursor-pointer block text-sm mb-1">Visibility</label>
+                                <select name="visibility" id="visibility" class="w-full rounded border border-gray-600 py-2">
+                                    <option value="">Select visibility</option>
+                                    <option value="public">Public</option>
+                                    <option value="privet">Privet</option>
+                                </select>
+
+                                @error('visibility')
+                                    <p class="text-red-700 text-sm">{{$message}}</p>
+                                @enderror
+
+                            </div>
+
+                            <div class="flex-1 mr-4">
+                                <label for="tag" class="cursor-pointer block text-sm mb-1">Tags</label>
+                                <input type="text" name="tag" id="tag" class="w-full rounded border border-gray-600 py-2" value="{{ old('tag')}}">
+
+                                @error('tag')
+                                    <p class="text-red-700 text-sm">{{$message}}</p>
+                                @enderror
+
+                            </div>
+                        </div>
+
+                        <div class="mt-6 flex">
+                            <div class="flex-1">
+                                <label for="description" class="cursor-pointer block text-sm mb-1">Description</label>
+                                <textarea name="description" id="description" rows="10" class="w-full rounded border border-gray-600 py-2">{{ old('description') }}</textarea>
+
+                                @error('description')
+                                    <p class="text-red-700 text-sm">{{ $message }}</p>
+                                @enderror
+
+                            </div>
                         </div>
 
 
+                        <div class="mt-6">
+                            <button type="submit" class="bg-blue-800 px-4 py-2 text-white rounded-md text-base uppercase">Create</button>
+                        </div>
+                    </form>
 
-                    <div class="mt-6">
-                        <button type="submit"
-                            class="bg-emerald-800 px-4 py-2 text-white rounded-md text-base uppercase">Create</button>
-                    </div>
-                </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 
         </div>
         <!-- End Recent Sales -->
